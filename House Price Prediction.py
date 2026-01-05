894e829baf09c2cd960a465bc8e80dd0c226f7d8
Navya1812-coder <navyaluthra17@gmail.com>
2026-01-05 15:56:13 +0530

    Create House Price Prediction.py

---
+++ House Price Prediction.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+DATA_PATH = ""data/house_price_dataset.csv"
+if not os.path.exists(DATA_PATH):
+    raise FileNotFoundError(
+        "Dataset not found. Please download from Kaggle (see README)."
+    )
+  data = pd.read_csv(DATA_PATH)
+
+X = data[['sqft', 'bedrooms', 'bathrooms']]
+y = data['price']
